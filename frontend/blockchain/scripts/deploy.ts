6733675cc38d8fa95a5d1d38757e7d63614825d4
GETO.finance (GETO) <74251667+GETOfinance@users.noreply.github.com>
2025-03-21 17:32:43 +0300

    Add files via upload

--- frontend/blockchain/scripts/deploy.ts
+++ frontend/blockchain/scripts/deploy.ts
@@ -1,18 +1,17 @@
-import { network } from "hardhat";
-import { ethers } from "ethers";
+import { network, ethers } from "hardhat";
 
 async function main() {
 
   // Log the network name and chain ID
   console.log("Network Name:", network.name);
-  console.log("Network Chain ID:", (await ethers.provider.getNetwork()).chainId);
+  console.log("Network Chain ID:", network.config.chainId);
 
   const [deployer] = await ethers.getSigners(); // Gets deployer from hardhat.config.ts
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const balance = await deployer.provider.getBalance(deployer.address);
-  console.log("Account balance:", ethers.formatEther(balance), "ETH");
+  const balance = await ethers.provider.getBalance(deployer.address);
+  console.log("Account balance:", ethers.formatEther(balance), "S");
 
   const TradingAlgoAVS = await ethers.getContractFactory("TradingAlgoAVS");
   const contract = await TradingAlgoAVS.deploy();
